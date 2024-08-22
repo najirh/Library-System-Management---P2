0515172aa3a9acc4ea8baf23b16c83526f500099
Najir Hussain <business.najir@gmail.com>
2024-08-22 23:10:14 +0530

    Add files via upload

---
+++ solution day 1.sql
@@ -0,0 +1,154 @@
+SELECT * FROM books;
+SELECT * FROM branch;
+SELECT * FROM employees;
+SELECT * FROM issued_status;
+SELECT * FROM return_status;
+SELECT * FROM members;
+
+
+-- Project Task
+
+-- Task 1. Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+
+INSERT INTO books(isbn, book_title, category, rental_price, status, author, publisher)
+VALUES
+('978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.');
+SELECT * FROM books;
+
+
+-- Task 2: Update an Existing Member's Address
+
+UPDATE members
+SET member_address = '125 Main St'
+WHERE member_id = 'C101';
+SELECT * FROM members;
+
+
+-- Task 3: Delete a Record from the Issued Status Table 
+-- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+
+SELECT * FROM issued_status
+WHERE issued_id = 'IS121';
+
+DELETE FROM issued_status
+WHERE issued_id = 'IS121'
+
+
+
+-- Task 4: Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
+
+SELECT * FROM issued_status
+WHERE issued_emp_id = 'E101';
+
+
+-- Task 5: List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
+
+
+
+SELECT 
+    ist.issued_emp_id,
+     e.emp_name
+    -- COUNT(*)
+FROM issued_status as ist
+JOIN
+employees as e
+ON e.emp_id = ist.issued_emp_id
+GROUP BY 1, 2
+HAVING COUNT(ist.issued_id) > 1
+
+
+-- CTAS
+-- Task 6: Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
+
+CREATE TABLE book_cnts
+AS    
+SELECT 
+    b.isbn,
+    b.book_title,
+    COUNT(ist.issued_id) as no_issued
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1, 2;
+
+
+SELECT * FROM
+book_cnts;
+
+
+
+-- Task 7. Retrieve All Books in a Specific Category:
+
+SELECT * FROM books
+WHERE category = 'Classic'
+
+    
+-- Task 8: Find Total Rental Income by Category:
+
+
+SELECT
+    b.category,
+    SUM(b.rental_price),
+    COUNT(*)
+FROM books as b
+JOIN
+issued_status as ist
+ON ist.issued_book_isbn = b.isbn
+GROUP BY 1
+
+
+-- List Members Who Registered in the Last 180 Days:
+
+SELECT * FROM members
+WHERE reg_date >= CURRENT_DATE - INTERVAL '180 days'    
+    
+
+INSERT INTO members(member_id, member_name, member_address, reg_date)
+VALUES
+('C118', 'sam', '145 Main St', '2024-06-01'),
+('C119', 'john', '133 Main St', '2024-05-01');
+
+
+
+-- task 10 List Employees with Their Branch Manager's Name and their branch details:
+
+SELECT 
+    e1.*,
+    b.manager_id,
+    e2.emp_name as manager
+FROM employees as e1
+JOIN  
+branch as b
+ON b.branch_id = e1.branch_id
+JOIN
+employees as e2
+ON b.manager_id = e2.emp_id
+
+
+-- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold 7USD:
+
+CREATE TABLE books_price_greater_than_seven
+AS    
+SELECT * FROM Books
+WHERE rental_price > 7
+
+SELECT * FROM 
+books_price_greater_than_seven
+
+
+-- Task 12: Retrieve the List of Books Not Yet Returned
+
+SELECT 
+    DISTINCT ist.issued_book_name
+FROM issued_status as ist
+LEFT JOIN
+return_status as rs
+ON ist.issued_id = rs.issued_id
+WHERE rs.return_id IS NULL
+
+    
+SELECT * FROM return_status
+
+
+
